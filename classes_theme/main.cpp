a08f86867f8af99abb8c2165a23da52c57cd8bd7
Grigoriy A. Rybakin <grigoriy71215@gmail.com>
2025-10-04 12:41:01 +0500

    adding the comments to ClassTheme project

--- classes_theme/main.cpp
+++ classes_theme/main.cpp
@@ -1,14 +1,14 @@
 #include "./include/MyClass.h" // библиотека данных для потока вывода MyClass
 #include <iostream>            // библиотека данных для потока вывода cout
 
-void print(MyClass *myClass) { //
-  std::cout << myClass->getI() << '\n';
+void print(MyClass *myClass) {          // выводим myClass
+  std::cout << myClass->getI() << '\n'; // выводим myClass->getI()
 }
 
-int main() { // записывае функци
-  MyClass *myClass = new MyClass(42);
-  print(myClass);
-  myClass->setI(10);
+int main() { // записываем функцию с которой начинает программа
+  MyClass *myClass = new MyClass(42); //
+  print(myClass);                     //
+  myClass->setI(10);                  // выводим на экран 10
   print(myClass);
   delete myClass;
   return 0;
