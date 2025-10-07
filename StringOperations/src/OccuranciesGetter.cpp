28be31916b199dca9263616006c6cfc55a050b55
Grigoriy A. Rybakin <grigoriy71215@gmail.com>
2025-10-07 08:51:33 +0500

    add comments to OccuranciesGetter class

--- StringOperations/src/OccuranciesGetter.cpp
+++ StringOperations/src/OccuranciesGetter.cpp
@@ -1,22 +1,27 @@
 #include "../include/OccuranciesGetter.h"
-#include <iostream>
 
 OccuranciesGetter::OccuranciesGetter(const std::string &ourString,
-                                     const std::string &subString) {
-  this->ourString = ourString;
-  this->subString = subString;
+                                     const std::string &subString) { // строка
+  // и подстрока, в которых мы ищем число вхождений
+
+  this->ourString = ourString; // cтрока
+  this->subString = subString; // подстрока
 }
 
-int OccuranciesGetter::getCountOfOccurancies() {
-  if (stringModified) {
-    getOccurancies();
-    stringModified = false;
+int OccuranciesGetter::getCountOfOccurancies() { // функция поиска совпадения
+  // строки и подстроки возвращает номер символа в строке, с которого есть
+  // совподение
+  if (stringModified) {     // проверяет переменную OccuranciesGetter
+    getOccurancies();       // реализация функции
+    stringModified = false; // изменяем функцию
   }
-  return countOfOccurrences;
+  return countOfOccurrences; // возвращает countOfOccurrences
 }
 
-void OccuranciesGetter::getOccurancies() {
-  countOfOccurrences = iterate(this, &OccuranciesGetter::getInclusion, 0, 0);
+void OccuranciesGetter::getOccurancies() { // возвращает количество вхождений
+  // (OccuranciesGetter)
+  countOfOccurrences = iterate(this, &OccuranciesGetter::getInclusion, 0,
+                               0); // cоздаем ссылку на OccuranciesGetter
 }
 
 int OccuranciesGetter::getInclusion(
@@ -31,21 +36,24 @@ int OccuranciesGetter::iterate(OccuranciesGetter *obj,
                                int pos) { // функция высокого
   // порядка (в параметрах есть указатель на функцию) для реализации способа
   // подсчета того, что находит функция по указателю
-  while (pos != std::string::npos) { // цикл нахождения количества вхождений,
-                                     // возвращаемых функцией по указателю
-    pos = (obj->*stepFunc)(value, pos);
-    if (pos != std::string::npos) { // условие нахождения позиции по задаче,
-      // которую решает функция по указателю
-      ++value;
-      ++pos;
+  while (pos != std::string::npos) {    // цикл нахождения количества вхождений,
+                                        // возвращаемых функцией по указателю
+    pos = (obj->*stepFunc)(value, pos); // вызов функции stepFunc у объекта
+                                        // obj, результат поместить в pos
+    if (pos != std::string::npos) {     // условие нахождения позиции по задаче,
+                                        // которую решает функция по указателю
+      ++value;                          // увеличиваем value
+      ++pos;                            // увеличиваем pos
     }
   }
-  return value;
+  return value; // выводим value
 }
 
-void OccuranciesGetter::setStrings(const std::string &ourString,
-                                   const std::string &subString) {
-  this->ourString = ourString;
-  this->subString = subString;
-  stringModified = true;
+void OccuranciesGetter::setStrings(
+    const std::string &ourString,
+    const std::string &subString) { // строка и подстрока, в которых мы ищем
+                                    // число вхождений
+  this->ourString = ourString;      // cтрока
+  this->subString = subString;      // подстрока
+  stringModified = true;            // изменяем функцию
 }
