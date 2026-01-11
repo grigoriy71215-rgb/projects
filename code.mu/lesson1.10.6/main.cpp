fd2304183d599fadde5aad2cdb5dcf4ac1f783e5
Grigory A. Rybakin <grigoriy71215@gmail.com>
2026-01-11 18:06:12 +0500

    add projects lesson1.10.4, lesson1.10.5

---
+++ code.mu/lesson1.10.6/main.cpp
@@ -0,0 +1,8 @@
+#include <cmath>
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+  int num = 10000;
+  std::cout << "Степень числа 10 от " << num << ": " << log10(num) << '\n';
+  return 0;
+}
